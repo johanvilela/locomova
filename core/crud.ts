7a718228875849d3428378e6bdec8473be055bae
Johan Vilela <30961343+johanvilela@users.noreply.github.com>
2023-08-04 13:36:17 +0000

    feat: add read() function

--- core/crud.ts
+++ core/crud.ts
@@ -3,11 +3,46 @@ const DB_FILE_PATH = "./core/db";
 
 console.log("[Amazing CRUD]");
 
+interface Todo {
+    date: string;
+    content: string;
+    done: boolean;
+}
+
 function create(content: string) {
+    const todo: Todo = {
+        date: new Date().toISOString(),
+        content: content,
+        done: false,
+    }
+
+    const todos: Array<Todo> = [
+        ...read(),
+        todo,
+    ]
+
     // save content on system
-    fs.writeFileSync(DB_FILE_PATH, content);
+    fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
+        todos,
+    }, null, 2));
     return content;
 }
 
+function read(): Array<Todo> {
+    const dbString = fs.readFileSync(DB_FILE_PATH, "utf-8");
+    const db = JSON.parse(dbString || "{}");
+    if (!db.todos) {
+        return [];
+    }
+    return db.todos;
+}
+
+function CLEAR_DB() {
+    fs.writeFileSync(DB_FILE_PATH, "");
+}
+
 // [SIMULATION]
-console.log(create("Task I'll do today!"));
+CLEAR_DB();
+create("Fisrt TODO");
+create("Second TODO");
+console.log(read());
