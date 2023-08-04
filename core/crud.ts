9564b8b67b3a30c99153d281a33800b253472421
Johan Vilela <30961343+johanvilela@users.noreply.github.com>
2023-08-04 12:23:44 +0000

    refactor: add typescript at project

--- core/crud.ts
+++ core/crud.ts
@@ -1,9 +1,9 @@
-const fs = require("fs");
+import fs from "fs";
 const DB_FILE_PATH = "./core/db";
 
 console.log("[Amazing CRUD]");
 
-function create(content) {
+function create(content: string) {
     // save content on system
     fs.writeFileSync(DB_FILE_PATH, content);
     return content;
