5a222ebd5cc1309ac01139e0f823e04ec3882458
Johan Vilela <johanvilela@hotmail.com>
2024-02-01 10:07:08 -0300

    feat: add `create` function at api

--- pages/api/todos.ts
+++ pages/api/todos.ts
@@ -11,6 +11,12 @@ export default function handler(
     todoController.get(request, response);
     return;
   }
+
+  if (request.method === "POST") {
+    todoController.create(request, response);
+    return;
+  }
+
   response.status(405).json({
     message: "Method not allowed",
   });
