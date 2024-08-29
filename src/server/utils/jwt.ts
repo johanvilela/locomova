37c2951029c4c693fc503dcedb2e01e3b2eecb4d
Johan Vilela <johanvilela@hotmail.com>
2024-08-29 15:26:28 -0300

    feat(auth): create `verifyToken()` function

--- src/server/utils/jwt.ts
+++ src/server/utils/jwt.ts
@@ -1,4 +1,5 @@
 import * as jose from "jose";
+import { NextApiRequest } from "next";
 
 export async function generateAccessToken(username: string) {
   const secret = new TextEncoder().encode(process.env.JWT_SECRET);
@@ -11,4 +12,21 @@ export async function generateAccessToken(username: string) {
     .sign(secret);
 
   return jwt;
+}
+
+export async function verifyToken(request: NextApiRequest) {
+  const authorization = request.headers.authorization;
+
+  if (!authorization) {
+    throw new Error("Missing `Authorization` request header");
+  }
+
+  const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+  const jwt = authorization.split(" ")[1];
+
+  try {
+    await jose.jwtVerify(jwt, secret);
+  } catch (error) {
+    throw new Error("Unauthorized user");
+  }
 }
